6e962c540c56907e28aaea710e3c9d29396daa40
nikhil <nikhilshah525@gmail.com>
2022-01-24 18:12:30 +0530

    design

---
+++ app/src/main/java/com/example/design/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.design
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+
+   lateinit var constraintLayout:ConstraintLayout
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
